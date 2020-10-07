64aa3618a179a72120f46b4067cdb7ac9bcc3f26
hanjiung <smert2580@naver.com>
2020-10-08 02:20:13 +0900

    add src

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ machine learning
 
 It is a space to collect the contents studied using Tensor Flow.
 
-
-
 😁
---
+++ image/chapter1/imageLoad.py
@@ -0,0 +1,19 @@
+import numpy as np
+#행렬을 다루기 위한 모듈
+
+import os
+# 운영 체제 인터페이스 모듈
+
+import PIL
+# Python image Library 모듈
+
+import tensorflow as tf
+#import tensorflow_datasets as tfds
+
+import tensorflow_datasets as tfds
+# tensorflow dataset을 불러오기 위함
+
+print(tf.__version__)
+# tensorlfow 버전 출력
+
+
